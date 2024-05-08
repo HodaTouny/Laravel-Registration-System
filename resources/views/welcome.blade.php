8e3a02f6d3e909bd017370b2add1be3c81f7544c
Hoda Touny <100847847+HodaTouny@users.noreply.github.com>
2024-05-08 13:16:22 +0300

    add tab icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title','HomePage')</title>
     <link rel="stylesheet" href="{{ URL('BootstrapCss/bootstrap.min.css') }}">
+    <link rel="icon" href="{{URL('assets/usicon.png')}}">
 </head>
 <body>
     <header>
